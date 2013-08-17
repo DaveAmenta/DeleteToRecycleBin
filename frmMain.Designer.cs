648c27f6fde3da769a255fd0c1856abc50a24c4b
Dave Amenta <dave@daveamenta.com>
2013-08-17 13:16:08 -0700

    Initial - old code import - sample app

---
+++ frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DeleteToRecycleBin
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnRecycle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OFD
+            // 
+            this.OFD.Filter = "*.*|All Files";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(151, 38);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(87, 27);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select file...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(370, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnRecycle
+            // 
+            this.btnRecycle.Location = new System.Drawing.Point(123, 82);
+            this.btnRecycle.Name = "btnRecycle";
+            this.btnRecycle.Size = new System.Drawing.Size(141, 46);
+            this.btnRecycle.TabIndex = 2;
+            this.btnRecycle.Text = "Recycle";
+            this.btnRecycle.UseVisualStyleBackColor = true;
+            this.btnRecycle.Click += new System.EventHandler(this.btnRecycle_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 140);
+            this.Controls.Add(this.btnRecycle);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Recycler";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnRecycle;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// David Amenta - DaveAmenta.com
+// 05/05/08
+
+namespace DeleteToRecycleBin
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnRecycle.Enabled = false;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OFD.Filter = "All Files|*.*";
+            OFD.ShowDialog();
+            OFD.Multiselect = false;
+
+            if (OFD.FileName != "")
+            {
+                if (System.IO.File.Exists(OFD.FileName))
+                {
+                    txtFile.Text = OFD.FileName;
+                    btnRecycle.Enabled = true;
+                }
+            }
+        }
+
+        private void btnRecycle_Click(object sender, EventArgs e)
+        {
+            if (RecybleBin.Send(txtFile.Text))
+            {
+                txtFile.Text = "";
+                btnRecycle.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("There was an error trying to move the file to the recycle bin", "Error");
+            }
+
+        }
+    }
+}
